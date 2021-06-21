f8ad5f23400c3bbda5623f76d0af86c8a0d037d0
Anis Abdellatif <anisabdellatif57@gmail.com>
2021-06-21 20:58:13 +0100

    oopsie

--- src/Command.js
+++ src/Command.js
@@ -16,28 +16,28 @@ class Command {
         }
 
         if (message.client.botConfig.disabledCmds.includes(this.name)) {
-            throw Errors.DisabledCmdError();
+            throw new Errors.DisabledCmdError();
         } else if (message.channel.type == "dm" && !this.dm) {
-            throw Errors.DmChannelError();
+            throw new Errors.DmChannelError();
         } else if (this.dev && message.author.id != message.client.dev) {
-            throw Errors.DevelopperError();
+            throw new Errors.DevelopperError();
         } else if (
             this.permissions &&
             !message.member.hasPermission(this.permissions)
         ) {
-            throw Errors.UserPermissionError();
+            throw new Errors.UserPermissionError();
         } else if (
             this.botpermissions &&
             !message.guild
                 .member(message.client.user)
                 .hasPermission(this.permissions)
         ) {
-            throw Errors.BotPermissionError();
+            throw new Errors.BotPermissionError();
         } else if (this.argsRules) {
             let val = new Validator(this.args, this.argsRules);
             if (val.fails()) {
                 // console.log(val.errors.all());
-                throw Errors.ArgumentsError();
+                throw new Errors.ArgumentsError();
             }
         }
 
